db139202c5b8746d4e73cedcf4d642e7601cbd0b
joaco <joaco@joaco-pc>
2019-11-02 02:26:25 -0300

    Falta generalizar para todos los leds lo que esta en el led 0 (5 minutos), aplicar lo de la tecla b y modularizar. Salu2.

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <allegro5/allegro.h> 
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h> 
+#include <allegro5/allegro_acodec.h> 
+
+    void turn_led_on(ALLEGRO_BITMAP * led_on, int location);
+
+    void turn_led_off(ALLEGRO_BITMAP * led_off, int location);
+
+    void initialize_leds(ALLEGRO_BITMAP * background, ALLEGRO_BITMAP * led_off);
+    
+    static int location[8] = {15,75,135,195,255,315,375,435};
+    
+    void make_sound (ALLEGRO_SAMPLE *sample);
 
 int main(int argc, char** argv) {
 
@@ -10,6 +22,7 @@ int main(int argc, char** argv) {
     ALLEGRO_BITMAP *led_on = NULL;
     ALLEGRO_BITMAP *background = NULL;
     ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    ALLEGRO_SAMPLE *sample = NULL;
 
     if (!al_init()) {
         fprintf(stderr, "failed to initialize allegro!\n");
@@ -47,22 +60,57 @@ int main(int argc, char** argv) {
     display = al_create_display(500, 100);
     if (!display) {
         al_destroy_bitmap(led_off);
+        al_destroy_bitmap(led_on);
         al_destroy_bitmap(background);
         fprintf(stderr, "failed to create display!\n");
         return -1;
     }
     
+    if (!al_install_audio()) {
+        fprintf(stderr, "failed to initialize audio!\n");
+        al_destroy_bitmap(led_off);
+        al_destroy_bitmap(led_on);
+        al_destroy_bitmap(background);
+        al_destroy_display(display);
+        return -1;
+    }
+
+    if (!al_init_acodec_addon()) {
+        fprintf(stderr, "failed to initialize audio codecs!\n");
+        al_destroy_bitmap(led_off);
+        al_destroy_bitmap(led_on);
+        al_destroy_bitmap(background);
+        al_destroy_display(display);
+        al_uninstall_audio();
+        return -1;
+    }
+
+    if (!al_reserve_samples(1)) {
+        fprintf(stderr, "failed to reserve samples!\n");
+        al_destroy_bitmap(led_off);
+        al_destroy_bitmap(led_on);
+        al_destroy_bitmap(background);
+        al_destroy_display(display);
+        return -1;
+    }
+
+    sample = al_load_sample("audio.wav");
+
+    if (!sample) {
+        printf("Audio clip sample not loaded!\n");
+        al_destroy_bitmap(led_off);
+        al_destroy_bitmap(led_on);
+        al_destroy_bitmap(background);
+        al_destroy_sample(sample);
+        al_uninstall_audio();
+        al_destroy_display(display);
+        al_destroy_event_queue(event_queue);
+        return -1;
+    }
     
     al_draw_bitmap(background,0 , 0, 0); 
     
-    int i;
-    for (i=0;i<8;i++){
-	static int x = 15;
-	al_draw_bitmap(led_off, x, 25, 0);
-	x+=60;
-	}
-    
-    al_flip_display();
+    initialize_leds(background, led_off);
    
     event_queue = al_create_event_queue();
     if(!event_queue) {
@@ -75,6 +123,7 @@ int main(int argc, char** argv) {
     al_register_event_source(event_queue, al_get_keyboard_event_source());
     
     bool done = false;
+    bool is_on[8] = {};
     
     while(!done){
         ALLEGRO_EVENT events;
@@ -86,8 +135,19 @@ int main(int argc, char** argv) {
                     al_flip_display();
                     break;
                 case ALLEGRO_KEY_1:
-                    al_clear_to_color(al_map_rgb(40,150,0));
-                    al_flip_display();
+                    if (is_on[0] == true) {
+                        turn_led_off(led_off,location[0]);
+                        al_flip_display();
+                        turn_led_off(led_off,location[0]);
+                        is_on[0]=false;
+                    }
+                    else {
+                        turn_led_on(led_on,location[0]);
+                        al_flip_display();
+                        turn_led_on(led_on,location[0]);
+                        is_on[0]=true;
+                    }
+                    make_sound(sample);
                     break;
                 case ALLEGRO_KEY_2:
                     al_clear_to_color(al_map_rgb(250,250,250));
@@ -117,3 +177,35 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
+
+    void turn_led_on(ALLEGRO_BITMAP * led_on, int location) {
+        al_draw_bitmap(led_on, location, 25, 0);
+    }
+
+    void turn_led_off(ALLEGRO_BITMAP * led_off, int location) {
+        al_draw_bitmap(led_off, location, 25, 0);
+    }
+
+    void initialize_leds(ALLEGRO_BITMAP * background, ALLEGRO_BITMAP * led_off){
+    
+    
+    al_draw_bitmap(background,0 , 0, 0); 
+    int i;
+    for (i=0;i<8;i++){
+	static int x = 15;
+        turn_led_off(led_off, x);
+	x+=60;
+	}
+    al_flip_display();
+    al_draw_bitmap(background,0 , 0, 0); 
+    for (i=0;i<8;i++){
+	static int x = 15;
+        turn_led_off(led_off, x);
+	x+=60;
+	}
+    }
+    
+    void make_sound (ALLEGRO_SAMPLE *sample){
+      al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+    }
